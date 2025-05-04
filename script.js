d5235d5c2a35597d33b6cf0e1ce80f8e7771fbd0
Mateo Monroy <monroymateo1@gmail.com>
2025-05-04 13:38:15 -0500

    listo

---
+++ script.js
@@ -0,0 +1,19 @@
+function descargarContacto() {
+    const vcardData = `BEGIN:VCARD
+VERSION:3.0
+N:Arango Ramirez;Claudia;Patricia;;
+FN:Claudia Patricia Arango Ramirez
+TEL:+57 3207868790
+EMAIL;TYPE=WORK: gcomercial@acerosymallas.com
+NOTE: Gerente Comercial
+END:VCARD`;
+
+    const blob = new Blob([vcardData], { type: 'text/vcard' });
+    const url = URL.createObjectURL(blob);
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = 'contacto.vcf';
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+}
